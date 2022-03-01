f76444471623eaa71dc3fb28be740dfadcacb7b3
vimcki <0xvimcki@gmail.com>
2022-03-01 20:16:16 +0100

    make createHarvestInstruction work

--- index.ts
+++ index.ts
@@ -5,6 +5,8 @@ import * as factory from "@staratlas/factory"
 import KeypairProvider from "./pkg/keypair/secret_key_file/keypair"
 
 const atlas_mint = new web3.PublicKey("ATLASXmbPQxBUYbxPsV97usA3fPQYEqzQBUHgiFCUsXx")
+const ship_mint = new web3.PublicKey("Ev3xUhc1Leqi4qR2E5VoG9pcxCvHHmnAaSRVPg485xAT")
+const score_program_id = new web3.PublicKey("FLEET1qqzpexyaDpqb2DGsSzE2sDCizewCg9WjrA6DBW")
 
 const kpp = new KeypairProvider("/home/user/.config/solana/bank.json")
 
@@ -14,12 +16,18 @@ const keypair = kpp.get()
 async function go() {
 	let connection = new web3.Connection(web3.clusterApiUrl('mainnet-beta'));
 
-	const my_atlas_account = await connection.getTokenAccountsByOwner(keypair.publicKey, {mint: atlas_mint})
-	console.log(my_atlas_account.value[0].pubkey.toJSON())
-	console.log(factory)
+	const resp = await connection.getTokenAccountsByOwner(keypair.publicKey, {mint: atlas_mint})
+	const my_atlas_account = resp.value[0].pubkey
 
-	//factory.createHarvestInstruction(connection, keypair.publicKey, )
+	const instruction = await factory.createHarvestInstruction(
+		connection, 
+		keypair.publicKey, 
+		my_atlas_account,
+		atlas_mint,
+		ship_mint,
+		score_program_id
+	)
 
-	//console.log(keypair.publicKey.toJSON())
+	console.log(instruction)
 }
 go()
