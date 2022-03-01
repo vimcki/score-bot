7ad32359e485c9e32a343b21d5b46cb2a1175b5e
vimcki <0xvimcki@gmail.com>
2022-03-01 21:11:00 +0100

    finish opalj harvest

--- index.ts
+++ index.ts
@@ -1,11 +1,11 @@
 import * as web3 from '@solana/web3.js';
 
-import {createHarvestInstruction} from "@staratlas/factory"
+import * as factory from "@staratlas/factory"
 
 import KeypairProvider from "./pkg/keypair/secret_key_file/keypair"
 
 const atlas_mint = new web3.PublicKey("ATLASXmbPQxBUYbxPsV97usA3fPQYEqzQBUHgiFCUsXx")
-const ship_mint = new web3.PublicKey("Ev3xUhc1Leqi4qR2E5VoG9pcxCvHHmnAaSRVPg485xAT")
+const opalj_mint = new web3.PublicKey("Ev3xUhc1Leqi4qR2E5VoG9pcxCvHHmnAaSRVPg485xAT")
 const score_program_id = new web3.PublicKey("FLEET1qqzpexyaDpqb2DGsSzE2sDCizewCg9WjrA6DBW")
 
 const kpp = new KeypairProvider("/home/user/.config/solana/bank.json")
@@ -19,15 +19,21 @@ async function go() {
 	const resp = await connection.getTokenAccountsByOwner(keypair.publicKey, {mint: atlas_mint})
 	const my_atlas_account = resp.value[0].pubkey
 
-	const instruction = await createHarvestInstruction(
+	const instructions = await factory.createHarvestInstruction(
 		connection, 
 		keypair.publicKey, 
 		my_atlas_account,
 		atlas_mint,
-		ship_mint,
+		opalj_mint,
 		score_program_id
 	)
-
-	console.log(instruction)
+	let transaction = new web3.Transaction()
+	transaction.add(instructions[0])
+	const signature = await web3.sendAndConfirmTransaction(
+		connection,
+		transaction,
+		[keypair],
+  );
+  console.log('SIGNATURE', signature);
 }
 go()
